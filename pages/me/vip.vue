7487f9fa63a451929831b60c6eba0aaa21667261
zhangyazhou <2517177019@qq.com>
2024-04-23 16:50:28 +0800

    feat: 开通VIP套餐

--- pages/me/vip.vue
+++ pages/me/vip.vue
@@ -96,7 +96,8 @@ export default {
   onLoad() {
     this.getVipList()
   },
-  onShow() {
+  async onShow() {
+    await this.$store.dispatch('getUserInfo')
     if (this.userInfo.isVip) uni.switchTab({ url: '/pages/me/me' })
   },
   methods: {
